2d2ca12e4c3d3804c5d09d87d7e26c68d823c83f
513 <513@DESKTOP-1JIOJDN>
2018-05-26 11:06:08 +0900

    마이바티스 연동테스트

---
+++ mybatis_test01/src/org/mybatis/example/SaramServiceClient.java
@@ -0,0 +1,27 @@
+package org.mybatis.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SaramServiceClient {
+
+	public static void main(String[] args) throws IOException {
+		String resource = "mybatis-config.xml";
+
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+			// do work
+			SqlSession session = sqlSessionFactory.openSession();
+			SaramDto saram = session.selectOne("SaramMapper.selectSaram", "HONG");
+			System.out.println(saram);
+		
+			session.close();
+	}
+
+}
