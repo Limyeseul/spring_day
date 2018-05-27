3e609995a32ce16c3e89ffe3da855436572910c9
yslim <yslim@218.38.137.27>
2018-05-27 18:44:12 +0900

    sql 추

---
+++ mybatis_test01/src/sql.sql
@@ -0,0 +1,16 @@
+-- SARAM
+/*
+CREATE TABLE SARAM (
+					ID VARCHAR(10)
+				  , NAME VARCHAR(10)
+				  , AGE VARCHAR(5)
+);
+*/
+
+
+INSERT INTO SARAM (ID
+				 , NAME
+				 , AGE)
+		VALUES ('HONG'
+			   ,'GILDONG'
+			   ,'30');
